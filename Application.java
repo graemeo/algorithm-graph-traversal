366c2fb5e22ce2d76c6898f58efda017ff4c6c1b
Graeme O <graeme.ong>
2018-01-20 15:56:02 +1100

    [GraemeO] First cut to experiment graph algo to include BFS to find a vertex for a given adjacency list

---
+++ Application.java
@@ -0,0 +1,94 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Application
+{
+    /*
+    Graph (Undirected)
+    0 -> 1, 2
+    1 -> 0, 2, 6
+    2 -> 0, 1, 3, 4
+    3 -> 2, 5, 6
+    4 -> 2, 5
+    5 -> 3, 4
+    6 -> 1, 3
+    
+    0 -- 1 -- 6   
+     \   |    |
+      -- 2 -- 3 -- 5
+          \       /
+           -- 4 --
+    
+    */
+
+    public static void main(String[] args) {
+        List<List<Integer>> adjacencyList = getAdjacencyList();
+        printAdjacencyList(adjacencyList);
+
+        // BFS
+        breadthFirstSearch(adjacencyList, 3);
+         
+        
+    }
+
+    public static void breadthFirstSearch(List<List<Integer>> adjacencyList, int number) {
+        int steps = 0;
+        boolean[] explored = new boolean[adjacencyList.size()];
+        Queue<Integer> queue = new LinkedList<Integer>();
+        queue.add(0);
+
+        while(!queue.isEmpty()) {
+            steps++;
+            Integer vertex = queue.remove();
+            explored[vertex] = true;
+            System.out.println("AT node: " + vertex);
+
+            if (vertex == number) {
+                System.out.println("FOUND! Number of steps: " + steps);
+                break;
+            }
+
+            for(Integer v : adjacencyList.get(vertex)) {
+                if (!explored[v]) {
+                    queue.add(v);
+                }
+            }
+        }
+        // Loop through queue when not empty
+        // Is found matching node?
+        // Add children to queue (if not explored)
+
+        
+    }
+    
+    public static  List<List<Integer>> getAdjacencyList() {
+        List<List<Integer>> adjacencyList = new ArrayList<List<Integer>>();
+        adjacencyList.add(Arrays.asList(1,2));
+        adjacencyList.add(Arrays.asList(0, 2, 6));
+        adjacencyList.add(Arrays.asList(0, 1, 3, 4));
+        adjacencyList.add(Arrays.asList(2, 5, 6));
+        adjacencyList.add(Arrays.asList(2, 5));
+        adjacencyList.add(Arrays.asList(3, 4));
+        adjacencyList.add(Arrays.asList(1, 3));
+
+        return adjacencyList;
+    }
+
+    public static void printAdjacencyList(List<List<Integer>> adjacencyList) {
+        int index = 0;
+
+        for(List<Integer> adjacency : adjacencyList) {
+            System.out.print(index + " -> ");
+            for(Integer vertex : adjacency) {
+                System.out.print(vertex + " ");
+            }
+            System.out.println("");
+            index++;
+        }
+
+    }
+
+}
